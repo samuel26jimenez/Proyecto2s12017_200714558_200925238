e293bd06c77ee7ce5fac17ef43305dea6adf0e17
samuel26jimenez <200925238@ingenieria.usac.edu.gt>
2017-04-13 03:04:07 -0600

    doble
    agreando usuarios a lista doble

--- Proyecto2_Edd/Estructuras_Python/CalendarDriver/ld_b_avl.py
+++ Proyecto2_Edd/Estructuras_Python/CalendarDriver/ld_b_avl.py
@@ -1,6 +1,33 @@
 __author__ = 'Samuel'
+#------ Lista Doblemente Enlazada --------
+class ListaD_Nodo:
+    def __init__(self, nom):
+        self.nom = nom
+        self.ante = None
+        self.sig = None
 
-class samuel (self, a, b):
-    self.a = a
-    self.b = b
+    def set_Nomb(self, nombre):
+        self.nom = nombre
 
+    def get_Nomb(self, nombre):
+        self.nom = nombre
+
+class Metodo_LD:
+    def __init__(self):
+        self.ini = None
+        self.fin = None
+
+    def esVacio(self):
+        if self.ante == None & self.sig == None:
+            return True
+        else:
+            return False
+
+    def inserta_Nodo(self, x):
+        nvo_Nodo = ListaD_Nodo(x)
+        if self.esVacio() == True:
+            self.ini = self.fin = nvo_Nodo
+        else:
+            self.fin.sig = nvo_Nodo
+            nvo_Nodo.ante = self.fin
+            self.fin = self.fin.sig
